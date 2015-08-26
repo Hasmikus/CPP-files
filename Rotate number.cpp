ac18a97dc228dc44237674362aec82d32fd37a98
Hasmikus <hasmikus@gmail.com>
2015-08-26 14:34:58 +0400

    Not working version.

--- Rotate number.cpp
+++ Rotate number.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-int main (){
-    int balance, number, newnumber;
+int rotation(int ); 
+int rotatednumb;
+
+int main(){ 
+    int numb; 
     cout<<"\nInput any integer number ";
-    cin>>number;
-    newnumber=0;
-    while (balance!=0) {
-          balance=number%10;
-          newnumber+=balance;
-          number=(number-balance)/10;
-          if (number%10!=0) {newnumber*=10;} 
-          }
-    cout<<"\nThe rotate number is "<<newnumber;
-    getch();   
+    cin>>numb;  
+    cout<<"\nThe rotate number is "<<rotation(numb);
+    getch();
 }
+    int rotation(int number){ 
+         int balance;  
+         balance=number%10;  
+          if ((number-balance)/10!=0) { 
+                rotatednumb=balance*10+rotation((number-balance)/10);      
+          } else return rotatednumb;
+              
+    }
