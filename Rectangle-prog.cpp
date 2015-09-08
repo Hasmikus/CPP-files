566244ba9e97e9564542da908e39576dd82155c5
Hasmikus <hasmikus@gmail.com>
2015-09-08 09:27:55 +0400

    Latest version

--- Rectangle-prog.cpp
+++ Rectangle-prog.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <conio.h>
+#include <ncurses.h>
 #include "Rectangle.h"
 
 using namespace std;
 
 int main () {
 double width2, length2;
-	cout"\nInput width and length of rectangle. They must be double numbers in [0.0, 20.0].";
-	cin>>width>>length;
+	std::cout<<"\nInput width and length of rectangle. They must be double numbers in [0.0, 20.0].";
+	std::cin>>width2>>length2;
 	
-	Rectangle A(width2, length2);
+	Rectangle A;
 	A.set_length(length2);
 	A.set_width(width2);
 	
-	cout<<"\nWidth = "<<A.get_width;
-	cout<<"\nLength = "<<A.get_length;
-	cout<<"\nPerimeter = "<<A.perimeter;
-	cout<<"\nArea = "<<A.area;
+	std::cout<<"\nWidth = "<<A.get_width();
+	std::cout<<"\nLength = "<<A.get_length();
+	std::cout<<"\nPerimeter = "<<A.perimeter();
+	std::cout<<"\nArea = "<<A.area();
 
 getch();
 return 0;
