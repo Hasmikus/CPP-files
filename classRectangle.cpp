74632d88756eb556dd248aee42a278ddde42e193
Hasmikus <hasmikus@gmail.com>
2015-09-08 22:35:59 +0400

    Main program, that counting the perimeter and area of the first rectangle. Also it compare the first rectangle with another rectangle.

--- classRectangle.cpp
+++ classRectangle.cpp
@@ -1,81 +1,34 @@
 #include <iostream>
 #include <conio.h>
-#include <math.h>
+#include "point.h"
+#include "pointClass.cpp"
+#include "Rectangle_4_points.h"
+#include "Rectangle_4points.cpp"
+
+
 using namespace std;
 
-class Point {
-      public:
-             int x,y;
-      public: 
-              Point (int, int);     
-              int putx ();
-              int puty ();
-};
-
-Point::Point(int x1, int  y1) { 
-      x=x1;
-      y=y1; 
-      }
-int Point::puty () { 
-      return y; 
-      }
-int Point::putx () { 
-      return x; 
-      }
-
-class Rectangle: public Point { 
-      float width, length;
-      public: 
-              Rectangle (int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4)
-                    :Point(x,y) {
-                    Point A(x1, y1);
-                    Point B(x2, y2);
-                    Point C(x3,y3);
-                    Point D(x4, y4);
-                    };
-              float perimeter(float width,float length);
-              float put_width(); 
-              float put_length();
-              void setWidth (int ,int , int , int );
-              void setLength (int ,int , int , int );
-              float area(float , float );
-              
-};
-
-void Rectangle::setWidth (int x1,int y1, int x2, int y2) {
-                   width=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-                   }  
-void Rectangle::setLength (int x1,int y1, int x4, int y4) {
-                   length=sqrt((x4-x1)*(x4-x1)+(y4-y1)*(y4-y1));
-                   }       
-float Rectangle::put_width(){
-                    return width;
-                   } 
-float Rectangle::put_length() {
-                    return length;
-                   }  
-float Rectangle::perimeter (float width,float length) { 
-                   return 2*(width+length); 
-                   }
-float Rectangle::area(float width,float length) {
-                    return width*length;  
-                   } 
 
 int main() {
     int x1,y1,x2,y2,x3,y3,x4,y4;
-    cout<<"\nInput coordinates for the 4 edges of rectangle. For example 0 1"<<endl;
+    int x1_2,y1_2,x2_2,y2_2,x3_2,y3_2,x4_2,y4_2;
+    cout<<"\nInput coordinates for  the 4 edges of first rectangle. For example 0 1"<<endl;
     cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
-    Rectangle R1(x1,y1,x2,y2,x3,y3,x4,y4);
-    R1.setWidth(x1, y1, x2, y2);
-    R1.setLength(x1,y1, x4, y4);
+    
+    cout<<"\nInput coordinates for the 4 edges of  second rectangle. For example 0 1"<<endl;
+    cin>>x1_2>>y1_2>>x2_2>>y2_2>>x3_2>>y3_2>>x4_2>>y4_2;
+    Point A(x1,y1), B(x2,y2), C(x3,y3), D(x4,y4);
+    Rectangle R1(A, B, C, D);
+    R1.setWidth(A, B) ;
+    R1.setLength(A, D);
 
 
     cout<<"----------------------"<<endl;
-    cout<<"The coordinates of rectangle edges: (" <<x1<<";"<<y1<<"),"<<"("<<x2<<";"<<y2<<"),";
-    cout<<"("<<x3<<";"<<y3<<"),"<<"("<<x4<<";"<<y4<<")"<<endl;
-    cout<<"Width ="<<R1.put_width()<<"  Length="<<R1.put_length()<<endl;
-    cout<<"Perimeter = "<<R1.perimeter(R1.put_width(), R1.put_length())<<endl;
-    cout<<"Area= "<<R1.area(R1.put_width(),R1.put_length())<<endl;
+    cout<<"The coordinates of rectangle edges: (" <<A.getx()<<";"<<A.gety()<<"),"<<"("<<B.getx()<<";"<<B.gety()<<"),";
+    cout<<"("<<C.getx()<<";"<<C.gety()<<"),"<<"("<<D.getx()<<";"<<D.gety()<<")"<<endl;
+    cout<<"Width ="<<R1.get_width()<<"  Length="<<R1.get_length()<<endl;
+    cout<<"Perimeter = "<<R1.perimeter(R1.get_width(), R1.get_length())<<endl;
+    cout<<"Area= "<<R1.area(R1.get_width(),R1.get_length())<<endl;
 
 getch();
 return 0;
